e9f0d1aafe35ea81506511b2e94a7956a0916bf3
Martin De Angelis <madeangelis@gmail.com>
2021-09-23 10:15:03 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,73 @@
+const api = {
+  key: "74d51720406910d84636a785614c9973",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(event) {
+  if (event.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>˚C</span>`;
+
+  let weather_el = document.querySelector(".current .weather");
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}˚c / ${Math.round(
+    weather.main.temp_max
+  )}˚c`;
+}
+
+function dateBuilder(todayDate) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[todayDate.getDay()];
+  let date = todayDate.getDate();
+  let month = months[todayDate.getMonth()];
+  let year = todayDate.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
